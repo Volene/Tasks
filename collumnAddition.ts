b91797ab2885357216d3d854aae0c25723ff92f8
Volene <44668364+Volene@users.noreply.github.com>
2020-03-30 10:33:58 +0300

    Update collumnAddition.ts

--- collumnAddition.ts
+++ collumnAddition.ts
@@ -1,7 +1,10 @@
 function columnAddition(...nums: number[]): number {
   let ten = 0;
   const result = [];
-  const [biggestNum] = nums.sort((a,b)=>b-a);
+  const biggestNum = nums.reduce(	  const [biggestNum] = nums.sort((a,b)=>b-a);
+    (max, n) => Math.max(max, n.toString().length),	
+    0	
+  );
   
   const formatedNums = nums.map(n =>
     [...n.toString().padStart(biggestNum, 0)].map(Number).reverse()
