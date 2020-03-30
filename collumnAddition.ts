27ad34c5c742aa1b794cb8453e07c78cb23b0f68
Volene <44668364+Volene@users.noreply.github.com>
2020-03-30 09:46:43 +0300

    Create collumnAddition.ts

---
+++ collumnAddition.ts
@@ -0,0 +1,24 @@
+function columnAddition(...nums: number[]): number {
+  let ten = 0;
+  let result = [];
+  const biggestNum = nums.reduce(
+    (max, n) => Math.max(max, n.toString().length),
+    0
+  );
+  const formatedNums = nums.map(n =>
+    [...n.toString().padStart(biggestNum, 0)].map(Number).reverse()
+  );
+  for (let i = 0; i < biggestNum; i++) {
+    let sum = 0;
+    let remOfDivByTen = 0;
+    formatedNums.forEach(numAr => (sum += numAr[i]));
+    if (i === biggestNum - 1) {
+      result.push(sum + ten);
+      return Number(result.reverse().join(""));
+    }
+    sum += ten;
+    remOfDivByTen = Number(sum.toString().slice(-1));
+    result.push(remOfDivByTen);
+    ten = (sum - remOfDivByTen) / 10;
+  }
+}
